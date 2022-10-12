6598eff7c2dc8a7f474e903c61be1ad5e0c78c22
Serhii <Kistanov.serhii@yandex.ru>
2022-10-12 12:48:40 +0300

    Delete lib directory

--- lib/domain/repository/day_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:meta/meta.dart';
-import '../../../domain/model/day.dart';
-
-abstract class DayRepository {
-  Future<Day> getDay({
-    @required double latitude,
-    @required double longitude,
-  });
-}
